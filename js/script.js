af754b0796097a61507b89d7f1537bdf5e464fd0
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2025-01-11 20:56:31 +0530

    Add sticky header

--- js/script.js
+++ js/script.js
@@ -34,3 +34,36 @@
       const savedTheme = localStorage.getItem("theme") || "light";
       setTheme(savedTheme);
   };
+
+
+  // Scroll sections
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    sections.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            // Active navbar links
+            navLinks.forEach((links) => {
+                links.classList.remove("active");
+                document
+                    .querySelector("header nav a[href*=" + id + "]")
+                    .classList.add("active");
+            });
+        }
+    });
+
+    // Sticky header
+    let header = document.querySelector("header");
+
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    // Remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+};
