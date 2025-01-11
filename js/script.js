89afede0e6a75d24fc19327e4e4a6ac5a58c528b
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2025-01-11 23:58:23 +0530

    Mobile and tablet responsive #1

--- js/script.js
+++ js/script.js
@@ -73,3 +73,13 @@ window.onscroll = () => {
     menuIcon.classList.remove("bx-x");
     navbar.classList.remove("active");
 };
+
+
+// Toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+};
